f35427e679dad607f747cee0520793a35fd61cb0
FleurD974 <fleur.dambreville@gmail.com>
2025-09-30 21:21:18 +0200

    create offer model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Offer(models.Model):
+    offer_name = models.CharField(max_length=100)
+    offer_price = models.FloatField(default=0.0)
+    offer_numberOfPerson = models.IntegerField(default=0)
+    offer_stock = models.IntegerField(default=0)
+    offer_description = models.TextField(blank=True)
+    
+    def __str__(self):
+        return f"{self.offer_name} ({self.offer_stock})"
